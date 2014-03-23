25915319878585847ba749a74530166abd0d4d6f
emacca <emacca@me.com>
2014-03-24 10:08:04 +1100

    rails new app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialproof::Application.config.secret_key_base = '3a4b9249332798c6dbca4ee13b0c00af18f36062f68cf799cfa20f755fb86f8b55c79920ce696938b53dfdfb92b358a6314e9957a370f49691a771fb3e84cee5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Socialproof::Application.config.session_store :cookie_store, key: '_socialproof_session'

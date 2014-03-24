3b6c77ef43441987449663728b3a5eaeb785d99e
emacca <emacca@me.com>
2014-03-24 12:00:49 +1100

    testing with user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,21 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  describe user do
+    
+    context 'should validate presence of' do
+      describe 'email' do
+        it { should validate_presence_of :email }
+      end
+      describe 'password' do
+        it { should validate_presence_of :password }
+      end
+      describe 'twitter' do
+        it { should validate_presence_of :twitter }
+      end
+    end
+
+  end
+
 end

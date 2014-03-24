10a424cc0351b76878ab2df1c194770e2ee9fff6
emacca <emacca@me.com>
2014-03-24 15:42:40 +1100

    addd twitter gem and made it show tweets of the user that is logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ActionController::Base
   def index
+    @user = current_user
+
     @users = User.all
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = "bOwOoiqzjlE56CtThcYQ"
+      config.consumer_secret = "1pd0CmGWsSZzxnidBgTjVmg5yUn7CwGBAyaYJnDdik"
+      config.access_token = "2278255818-gJb4PfUOVuJDeryfa8xNovArc2bxbfZozAhv80Q"
+      config.access_token_secret = "56Jy1uA01qJLsOyTUOmgpF9AQKOEtG4yVkyj4WFB2NCIT"
+    end
+
+    @data = client.user_timeline("#{@user.twitter}")
+
   end
 
 end

bf84d70c11dbb349a792f8432e60af7fa3eac731
emacca <emacca@me.com>
2014-03-26 10:20:23 +1100

    omniauth configuration and also routes for each callback

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Socialproof::Application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  get '/auth/:provider/callback', :to => 'users#set_twitter_token'
+  get '/auth/twitter/callback', :to => 'users#set_twitter_token'
+  get '/auth/instagram/callback', :to => 'users#set_instagram_token'
+
 
   get '/api/tweets' => 'pages#tweets'
   get '/api/pins' => 'pages#pins'
+  get '/api/grams' => 'pages#grams'
   # You can have the root of your site routed with "root"
   root 'pages#index'
 

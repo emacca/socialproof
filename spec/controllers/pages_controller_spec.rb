ddb5dc59c6fe21089b4f06e54dd44697c6385044
emacca <emacca@me.com>
2014-03-26 17:40:21 +1100

    space change

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -19,11 +19,14 @@ describe PagesController do
         :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/x-www-form-urlencoded; charset=UTF-8', 'User-Agent'=>'Twitter Ruby Gem 5.0.0.rc.1'}).
       to_return(:status => 200, :body => "", :headers => {})
 
-      
       get :tweets
       expect(response).to be_success
     end
   end 
+
+  describe "GET 'grams'" do
+    
+  end
 end
 
 
